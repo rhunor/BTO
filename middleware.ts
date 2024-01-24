5eb2b9a965aa0ce0780a723b9029e0fb249e1e72
rhunor <johndoe@example.com>
2024-01-24 15:31:17 +0100

    Your commit message here

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,23 @@
 import { withAuth } from "next-auth/middleware";
 
+interface Token {
+  id: number;
+  firstName: string;
+  lastName: string;
+  country: string;
+  countryCode: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  createdAt: Date;
+  updatedAt: Date;
+  role?: string; // Add the 'role' property here
+}
+
 export default withAuth({
   callbacks: {
-    authorized: async ({ req, token }) => {
+    authorized: async ({ req, token }: { req: any; token: Token }) => {
       if (req.nextUrl.pathname.startsWith("/admin")) return token?.role === "admin";
       return !!token;
     },
